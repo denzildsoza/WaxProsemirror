8b536c2a625573c9b47df118d8b84de686b0b195
DenzilDsouza <softsuave@Softsuaves-Mac-mini-2.local>
2023-03-18 18:51:34 +0530

    chapters-page-edit-chapters

---
+++ editors/demo/src/Editoria/dummy1.js
@@ -0,0 +1,73 @@
+export const dummy1 = `
+<table>
+		<thead>
+			<tr>
+				<th>Employee ID</th>
+				<th>Name</th>
+				<th>Department</th>
+				<th>Salary</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>001</td>
+				<td>John Smith</td>
+				<td>Accounting</td>
+				<td>$60,000</td>
+			</tr>
+			<tr>
+				<td>002</td>
+				<td>Jane Doe</td>
+				<td>Marketing</td>
+				<td>$70,000</td>
+			</tr>
+			<tr>
+				<td>003</td>
+				<td>Bob Johnson</td>
+				<td>IT</td>
+				<td>$80,000</td>
+			</tr>
+			<tr>
+				<td>004</td>
+				<td>Susan Lee</td>
+				<td>Human Resources</td>
+				<td>$65,000</td>
+			</tr>
+			<tr>
+				<td>005</td>
+				<td>Mike Brown</td>
+				<td>Sales</td>
+				<td>$75,000</td>
+			</tr>
+			<tr>
+				<td>006</td>
+				<td>Sarah Thompson</td>
+				<td>Finance</td>
+				<td>$85,000</td>
+			</tr>
+			<tr>
+				<td>007</td>
+				<td>David Kim</td>
+				<td>Engineering</td>
+				<td>$90,000</td>
+			</tr>
+			<tr>
+				<td>008</td>
+				<td>Lisa Hernandez</td>
+				<td>Customer Service</td>
+				<td>$55,000</td>
+			</tr>
+			<tr>
+				<td>009</td>
+				<td>Adam Taylor</td>
+				<td>Operations</td>
+				<td>$75,000</td>
+			</tr>
+			<tr>
+				<td>010</td>
+				<td>Karen Brown</td>
+				<td>Product Management</td>
+				<td>$95,000</td>
+			</tr>
+		</tbody>
+	</table>`;
